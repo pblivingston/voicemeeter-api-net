7846c4fafcf4f26c3e96e250525792601776a634
pblivingston <71585805+pblivingston@users.noreply.github.com>
2026-08-23 20:27:16 -0400

    Result<TResponse, TSent, TReturned> -> Result<TResponse, TParam, TValue>

--- VoicemeeterAPI/Types/Result.cs
+++ VoicemeeterAPI/Types/Result.cs
@@ -27,13 +27,17 @@ public static class Result
         => Result<TResponse, TValue>.Failure(response);
 
 
-    public static Result<TResponse, TSent, TReturned> Success<TResponse, TSent, TReturned>(TResponse response, TSent sent, TReturned returned)
+    public static Result<TResponse, TParam, TValue> Success<TResponse, TParam, TValue>(TResponse response, TParam param, TValue value)
         where TResponse : struct, Enum
-        => Result<TResponse, TSent, TReturned>.Success(response, sent, returned);
+        => Result<TResponse, TParam, TValue>.Success(response, param, value);
 
-    public static Result<TResponse, TSent, TReturned> Failure<TResponse, TSent, TReturned>(TResponse response, TSent sent)
+    public static Result<TResponse, TParam, TValue> Failure<TResponse, TParam, TValue>(TResponse response, TParam param, TValue value)
         where TResponse : struct, Enum
-        => Result<TResponse, TSent, TReturned>.Failure(response, sent);
+        => Result<TResponse, TParam, TValue>.Failure(response, param, value);
+
+    public static Result<TResponse, TParam, TValue> Failure<TResponse, TParam, TValue>(TResponse response, TParam param)
+        where TResponse : struct, Enum
+        => Result<TResponse, TParam, TValue>.Failure(response, param);
 }
 
 /// <summary>
@@ -163,77 +167,76 @@ public readonly struct Result<TResponse, TValue> : IEquatable<Result<TResponse, 
 }
 
 /// <summary>
-///   Represents the result of a VoicemeeterAPI operation that sends and returns specific values.
+///   Represents the result of a VoicemeeterAPI operation that sends or returns a specific value for a given parameter.
 /// </summary>
-public readonly struct Result<TResponse, TSent, TReturned> : IEquatable<Result<TResponse, TSent, TReturned>>
+public readonly struct Result<TResponse, TParam, TValue> : IEquatable<Result<TResponse, TParam, TValue>>
     where TResponse : struct, Enum
 {
-    private readonly Result<TResponse, TReturned> inner;
+    private readonly Result<TResponse, TValue> inner;
 
     /// <inheritdoc cref="Result{TResponse, TValue}.Response"/>
     public TResponse Response => this.inner.Response;
     public bool IsSuccess => this.inner.IsSuccess;
     public bool IsFailure => this.inner.IsFailure;
 
-    /// <summary>
-    ///   The actual value returned.
-    /// </summary>
-    /// <inheritdoc cref="Result{TResponse, TValue}.Value" path="/exception"/>
-    public TReturned Returned => this.inner.Value;
+    /// <inheritdoc cref="Result{TResponse, TValue}.Value"/>
+    public TValue Value => this.inner.Value;
 
     /// <summary>
-    ///   The actual value sent.
+    ///   The actual param sent.
     /// </summary>
-    public TSent Sent { get; }
+    public TParam Param { get; }
 
-    private Result(TResponse response, TSent sent, TReturned returned)
+    private Result(TResponse response, TParam param, TValue value, bool isSuccess)
     {
-        this.inner = (response, returned, true);
-        this.Sent = sent;
+        this.inner = (response, value, isSuccess);
+        this.Param = param;
     }
 
-    private Result(TResponse response, TSent sent)
+    private Result(TResponse response, TParam param)
     {
         this.inner = response;
-        this.Sent = sent;
+        this.Param = param;
     }
 
-    internal static Result<TResponse, TSent, TReturned> Success(TResponse response, TSent sent, TReturned returned)
-        => new(response, sent, returned);
-    internal static Result<TResponse, TSent, TReturned> Failure(TResponse response, TSent sent)
-        => new(response, sent);
+    internal static Result<TResponse, TParam, TValue> Success(TResponse response, TParam param, TValue value)
+        => new(response, param, value, true);
+    internal static Result<TResponse, TParam, TValue> Failure(TResponse response, TParam param, TValue value)
+        => new(response, param, value, false);
+    internal static Result<TResponse, TParam, TValue> Failure(TResponse response, TParam param)
+        => new(response, param);
 
-    public static implicit operator Result<TResponse, TSent, TReturned>((TResponse response, TSent sent, TReturned returned) t)
-        => new(t.response, t.sent, t.returned);
-    public static implicit operator Result<TResponse, TSent, TReturned>((TResponse response, TSent sent) t)
-        => new(t.response, t.sent);
+    public static implicit operator Result<TResponse, TParam, TValue>((TResponse response, TParam param, TValue value, bool isSuccess) t)
+        => new(t.response, t.param, t.value, t.isSuccess);
+    public static implicit operator Result<TResponse, TParam, TValue>((TResponse response, TParam param) t)
+        => new(t.response, t.param);
 
-    public static implicit operator TReturned(Result<TResponse, TSent, TReturned> result)
-        => result.Returned;
+    public static implicit operator TValue(Result<TResponse, TParam, TValue> result)
+        => result.Value;
 
-    public bool Equals(Result<TResponse, TSent, TReturned> other)
+    public bool Equals(Result<TResponse, TParam, TValue> other)
         => this.inner == other.inner
-        && EqualityComparer<TSent>.Default.Equals(this.Sent, other.Sent);
+        && EqualityComparer<TParam>.Default.Equals(this.Param, other.Param);
 
     public override bool Equals(object? obj)
-        => obj is Result<TResponse, TSent, TReturned> r
+        => obj is Result<TResponse, TParam, TValue> r
         && this.Equals(r);
 
     public override int GetHashCode()
 #if NET5_0_OR_GREATER
-        => HashCode.Combine(this.inner, this.Sent);
+        => HashCode.Combine(this.inner, this.Param);
 #else
     {
         unchecked
         {
             var hash = 17;
             hash = (hash * 23) + this.inner.GetHashCode();
-            hash = (hash * 23) + EqualityComparer<TSent>.Default.GetHashCode(this.Sent);
+            hash = (hash * 23) + EqualityComparer<TParam>.Default.GetHashCode(this.Param);
             return hash;
         }
     }
 #endif
 
-    public static bool operator ==(Result<TResponse, TSent, TReturned> a, Result<TResponse, TSent, TReturned> b) => a.Equals(b);
-    public static bool operator !=(Result<TResponse, TSent, TReturned> a, Result<TResponse, TSent, TReturned> b) => !a.Equals(b);
+    public static bool operator ==(Result<TResponse, TParam, TValue> a, Result<TResponse, TParam, TValue> b) => a.Equals(b);
+    public static bool operator !=(Result<TResponse, TParam, TValue> a, Result<TResponse, TParam, TValue> b) => !a.Equals(b);
 }
