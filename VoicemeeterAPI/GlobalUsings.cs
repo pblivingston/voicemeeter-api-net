64460209394283995fc961204eee220e6d198db8
pblivingston <71585805+pblivingston@users.noreply.github.com>
2026-08-30 11:55:46 -0400

    moved LockObject using

--- VoicemeeterAPI/GlobalUsings.cs
+++ VoicemeeterAPI/GlobalUsings.cs
@@ -1,8 +1,4 @@
 // Copyright (c) 2026 PBLivingston
 // SPDX-License-Identifier: MPL-2.0
 
-#if NET9_0_OR_GREATER
-global using LockObject = System.Threading.Lock;
-#endif
-
 global using PBLivingston.VoicemeeterAPI.Internal;
--- VoicemeeterAPI/Internal/LockObject.cs
+++ VoicemeeterAPI/Internal/LockObject.cs
@@ -1,10 +1,11 @@
 // Copyright (c) 2026 PBLivingston
 // SPDX-License-Identifier: MPL-2.0
 
+#if NET9_0_OR_GREATER
+global using LockObject = System.Threading.Lock;
+#else
 namespace PBLivingston.VoicemeeterAPI.Internal;
 
-#if NET9_0_OR_GREATER
-#else
 internal class LockObject
 {
     public Scope EnterScope()
